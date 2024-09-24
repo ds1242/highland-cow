42558bcada67ea0b28b4524b63bda9d399672845
David Shaw <david.shaw1242@gmail.com>
2024-09-23 20:10:06 -0400

    progress on setting up the initial servemux

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	godotenv.Load()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+
+	mux := http.NewServeMux()
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+	
 }
