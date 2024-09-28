cba49cf9b11685298a10450e4abf015d337e6c52
David Shaw <david.shaw1242@gmail.com>
2024-09-28 12:32:15 -0400

    started update user func

--- handler_user.go
+++ handler_user.go
@@ -72,6 +72,7 @@ func (cfg *apiConfig) handlerUserLogin(w http.ResponseWriter, r *http.Request) {
 	err := decoder.Decode(&params)
 	if err != nil {
 		RespondWithError(w, http.StatusBadRequest, "could not decode parameters")
+		return
 	}
 
 	ctx := r.Context()
@@ -79,16 +80,19 @@ func (cfg *apiConfig) handlerUserLogin(w http.ResponseWriter, r *http.Request) {
 	user, err := cfg.DB.GetUserByEmail(ctx, params.Email)
 	if err != nil {
 		RespondWithError(w, http.StatusBadRequest, "user not found")
+		return
 	}
 
 	passErr := auth.CheckPasswordHash(params.Password, user.Password)
 	if passErr != nil {
 		RespondWithError(w, http.StatusForbidden, "unable to login")
+		return
 	}
 
 	token, err := auth.CreateToken(user.ID.String(), defaultTokenExpiration, cfg.JWTSecret)
 	if err != nil {
 		RespondWithError(w, http.StatusUnauthorized, err.Error())
+		return
 	}
 
 	// convert queried DB user to a struct user
@@ -97,4 +101,20 @@ func (cfg *apiConfig) handlerUserLogin(w http.ResponseWriter, r *http.Request) {
 	userResponse := createUserResponse(convertedUser, token)
 	// Response
 	RespondWithJSON(w, http.StatusOK, userResponse)
+}
+
+func (cfg *apiConfig) handlerUserUpdate(w http.ResponseWriter, r *http.Request) {
+	type Params struct {
+		Name     string `json:"name,omitempty"`
+		Email    string `json:"email,omitempty"`
+		Password string `json:"password,omitempty"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := Params{}
+
+	err := decoder.Decode(&params)
+	if err != nil {
+
+	}
 }
