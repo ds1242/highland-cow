891ae8cfc8bb77968f4f05da12f663297c9ba2c1
David Shaw <david.shaw1242@gmail.com>
2024-10-04 16:20:48 -0400

    check and added a product

--- handler_scan_product.go
+++ handler_scan_product.go
@@ -4,6 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+	"database/sql"
+
+	"github.com/ds1242/highland-cow/internal/database"
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerScanProduct(w http.ResponseWriter, r *http.Request, user User, tokenString string) {
@@ -25,13 +30,23 @@ func (cfg *apiConfig) handlerScanProduct(w http.ResponseWriter, r *http.Request,
 
 	ctx := r.Context()
 
-	var firstProductScan bool
-
 	fmt.Println(params)
 	product, err := cfg.DB.GetProductByProductCode(ctx, params.ProductCode)
 	if err != nil {
-		firstProductScan = true
+		product, err = cfg.DB.AddProduct(ctx, database.AddProductParams{
+			ID: uuid.New(),
+			ProductName: params.ProductName,
+			Description: sql.NullString{String: params.Description, Valid: params.Description != ""},
+			Brand: sql.NullString{String: params.Brand, Valid: params.Brand != ""},
+			ProductCode: params.ProductCode,
+			CreatedAt: time.Now().UTC(),
+			UpdatedAt: time.Now().UTC(),
+		})
+		if err != nil {
+			RespondWithError(w, http.StatusBadRequest, "unable to find or add product")
+			return
+		}
 	}
 	fmt.Println(product)
-	fmt.Println(firstProductScan)
+
 }
