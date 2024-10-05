f7efa24fec05bb54f5f48b6c7c68e4da1e258e9c
David Shaw <david.shaw1242@gmail.com>
2024-10-05 07:40:02 -0400

    added a few comments to code

--- handler_user_scan_list.go
+++ handler_user_scan_list.go
@@ -5,14 +5,14 @@ import (
 )
 
 func (cfg *apiConfig) handlerGetUserScanList(w http.ResponseWriter, r *http.Request, user User, tokenString string) {
-
+	// Get user list slice from db
 	ctx := r.Context()
 	userList, err := cfg.DB.GetUserList(ctx, user.ID)
 	if err != nil {
 		RespondWithError(w, http.StatusBadRequest, "unable to get userlist")
 		return
 	}
-	// Setup func to convert to a json response struct
+	// Convert to a user list reponse for the json object to be returned 
 	var userListSlice []userListResponse
 	for _, listItem := range userList {
 		userListSlice = append(userListSlice, databaseUserListToUserList(listItem))
