9c90e22a0959312727648861d3adeb114501c907
David Shaw <david.shaw1242@gmail.com>
2024-10-07 20:42:38 -0400

    slight styling of the navbar

--- web-client/components/NavBrand.tsx
+++ web-client/components/NavBrand.tsx
@@ -13,10 +13,10 @@ export default function NavBrand() {
                 width={100}
                 height={100}
                 alt="brand image"
-                className="rounded-full p-2"
+                className="rounded-full p-2 hover:border-solid hover:border-2 hover:border-sky-200 ease-in-out"
 
             />
-            <h1 className="content-center p-2 text-3xl text-sky-500">
+            <h1 className="content-center p-2 text-3xl text-sky-500 hover:text-sky-700 ease-in">
                 The Highland Cow
             </h1>
         </Link>
