e6a81afb0adfb747e0ee129bdb7826eaa93d37f5
David Shaw <david.shaw1242@gmail.com>
2024-10-08 19:49:02 -0400

    started to create an object interface to allow links to be more flexible

--- web-client/components/NavBrand.tsx
+++ web-client/components/NavBrand.tsx
@@ -2,23 +2,25 @@ import Image from "next/image";
 import Link from "next/link";
 import brand from "../public/brand_cow.jpg"
 
-export default function NavBrand() {
+export default function NavBrand({ site_title }) {
     return (
-        <Link
-            href="/"
-            className="flex flex-row content-center"
-        >
+        <div className="flex flex-row content-center">
             <Image
                 src={brand}
                 width={100}
                 height={100}
                 alt="brand image"
-                className="rounded-full p-2 hover:border-solid hover:border-2 hover:border-sky-200 ease-in-out"
+                className="rounded-full p-2"
 
             />
-            <h1 className="content-center p-2 text-3xl text-sky-500 hover:text-sky-700 ease-in">
-                The Highland Cow
-            </h1>
-        </Link>
+            <Link
+                href="/"
+                className="flex flex-row"
+            >
+                <h1 className="content-center p-2 text-3xl text-sky-500 hover:text-sky-700 ease-in">
+                    { site_title }
+                </h1>
+            </Link>
+        </div>
     )
 }
--- web-client/components/NavLayout.tsx
+++ web-client/components/NavLayout.tsx
@@ -3,9 +3,17 @@ import NavBrand from "./NavBrand";
 import Link from "next/link";
 
 export default function NavLayout() {
+
+    const siteName: string = "The Highland Cow"
+    interface siteLinks {
+        url: string,
+        linkTitle: string
+    }
+
+
     return (
         <nav className="container flex flex-row">
-            <NavBrand />
+            <NavBrand site_title={siteName} />
             <div className="grow"></div>
             <Link href="/Login">Login</Link>
             <Link href="/Login">Sign Up</Link>
