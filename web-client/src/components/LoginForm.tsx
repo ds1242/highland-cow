cddd997b29b11694c4b63dc8caf60e2efcca5e47
David Shaw <david.shaw1242@gmail.com>
2024-10-16 06:19:16 -0400

    added some styling for mobile on the login form

--- web-client/src/components/LoginForm.tsx
+++ web-client/src/components/LoginForm.tsx
@@ -48,10 +48,10 @@ const LoginForm = () => {
 
 
     return (
-        <div className="bg-slate-200 border-2 border-slate-300 rounded-xl mx-auto min-h-48 shadow-lg">
-            <form className=" grid space-y-3 p-5 m-5 gap-3" onSubmit={handleFormSubmit}>
+        <div className="bg-slate-200 border-2 border-slate-300 rounded-xl mx-auto min-h-48 shadow-lg m-3">
+            <form className=" grid space-y-3 p-5 mt-5 mx-5 gap-3" onSubmit={handleFormSubmit}>
                 <input
-                    className="bg-slate-100 border-2 border-slate-300 text-slate-600 rounded-md p-1 shadow-inner focus:border-sky-500 focus:text-slate-600"
+                    className="bg-slate-100 border-2 border-slate-300 text-slate-600 rounded-md p-1 shadow-inner focus:border-sky-500 focus:text-slate-600 focus:outline-none"
                     id="email"
                     type="email"
                     name="email"
@@ -60,7 +60,7 @@ const LoginForm = () => {
                     onChange={handleChange}
                 />
                 <input
-                    className="bg-slate-100 border-2 border-slate-300 text-slate-600 rounded-md p-1 shadow-inner focus:border-sky-500 focus:text-slate-600"
+                    className="bg-slate-100 border-2 border-slate-300 text-slate-600 rounded-md p-1 shadow-inner focus:border-sky-500 focus:text-slate-600 focus:outline-none"
                     id="password"
                     type="password"
                     name="password"
@@ -71,7 +71,7 @@ const LoginForm = () => {
                 <button type="submit" className="mx-auto bg-slate-900 text-slate-100 w-28 h-14 rounded-md hover:bg-sky-600 shadow-md hover:shadow-slate-800">Submit</button>
                 <Link to="/signup" className="text-sky-900 hover:text-sky-600">Sign Up for an Account Here!</Link>
             </form>
-            { errorMessage && <div> {errorMessage} </div>}
+            { errorMessage && <div className="flex flex-row justify-center pb-3 text-red-500 italic"> {errorMessage} </div>}
         </div>
     )
 }
