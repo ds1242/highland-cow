a35ebd2e6a10e619a4474e243fa58e3d06a7f6e4
David Shaw <david.shaw1242@gmail.com>
2024-10-17 20:31:02 -0400

    rearrange my route for login

--- web-client/src/routes/Login.tsx
+++ web-client/src/routes/Login.tsx
@@ -1,14 +1,18 @@
+import Header from "../components/Header";
 import LoginForm from "../components/LoginForm"
+import Footer from "../components/Footer";
 
 function Login() {
     return (
-        <div className="bg-slate-400 h-full content-start py-3 lg:py-0">
-            <h1 className="flex justify-center text-4xl text-sky-900 py-2">Welcome!</h1>
-            {/* <div className=""> */}
+        <main className='bg-slate-400 h-lvh grid grid-flow-row'>
+            <Header />
+            <div className="bg-slate-400 h-full content-start py-3 lg:py-0">
+                <h1 className="flex justify-center text-4xl text-sky-900 py-2">Welcome!</h1>
                 <LoginForm />
-            {/* </div> */}
-        </div>
-        
+            </div>
+            <Footer />
+        </main>
+
     )
 }
 
