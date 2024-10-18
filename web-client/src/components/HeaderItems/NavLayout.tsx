90ccd8c8f37868a36cf7085fc9b61d0bc10ce7db
David Shaw <david.shaw1242@gmail.com>
2024-10-17 20:20:39 -0400

    user can log, token is stored, logout function to header has been added, swaps the header buttons

--- web-client/src/components/HeaderItems/NavLayout.tsx
+++ web-client/src/components/HeaderItems/NavLayout.tsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
 import NavBrand from "./NavBrand";
+import AuthService from '../../assets/auth'
 
 export default function NavLayout() {
 
+    const logout = (event:any) => {
+        event.preventDefault()
+        AuthService.logout()
+    }
 
+    const loggedIn = AuthService.loggedIn();
     interface siteLinks {
         id: string
         url: string,
         linkTitle: string
     }
-    const linkArr: Array<siteLinks> = [
+    const loggedOutArr: Array<siteLinks> = [
         {
             id: "0",
             url: "/login",
@@ -22,19 +28,35 @@ export default function NavLayout() {
         }
     ]
 
+    const loggedInArr: Array<siteLinks> = [
+        {
+            id: "0",
+            url: "/logout",
+            linkTitle: "Log Out"
+        }
+    ]
+
     return (
         <nav className="bg-slate-700 grid grid-rows-2 grid-cols-1 md:grid-rows-1 md:grid-cols-2">
             <div className="grow">
                 <NavBrand />
             </div>
             <div className="flex justify-center md:justify-end md:align-middle" >
-                {linkArr.map(function (link) {
-                    return (    
+                {!loggedIn ? loggedOutArr.map(function (link) {
+                    return (
                         <Link className="content-center text-sky-400 text-2xl md:text-2xl mx-3 hover:text-sky-100 ease-in" key={link.id} to={link.url}>
                             <button className="bg-slate-900 p-3">
                                 {link.linkTitle}
                             </button>
                         </Link>
+                    )
+                }) : loggedInArr.map(function (link) {
+                    return (
+                        <div className="content-center text-sky-400 text-2xl md:text-2xl mx-3 hover:text-sky-100 ease-in" key={link.id}>
+                            <button className="bg-slate-900 p-3" onClick={logout}>
+                                {link.linkTitle}
+                            </button>
+                        </div>
                     )
                 })}
             </div>
