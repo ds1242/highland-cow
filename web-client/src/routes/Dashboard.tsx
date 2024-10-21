92d5a408d7d8057960497116bd9f0db3dfa82437
David Shaw <david.shaw1242@gmail.com>
2024-10-21 18:44:21 -0400

    trying to layout the index page

--- web-client/src/routes/Dashboard.tsx
+++ web-client/src/routes/Dashboard.tsx
@@ -1,7 +1,28 @@
-
+// import auth from '../assets/auth'
+// import Dashboard from './Dashboard';
+// import { useAuth } from '../AuthProvider';
+// import { redirect, useNavigate } from 'react-router-dom';
+// import Login from './Login';
+// import { useState, useEffect } from 'react';
 
 export default function Dashboard() {
+    // const navigate = useNavigate();
+    // const loggedIn = useAuth();
+    // const [userId, setUserId] = useState<string | null>(null);
+
+    // useEffect(() => {
+    //     if (loggedIn) {
+    //         const id = auth.getId(); // Get the user ID from the token
+    //         setUserId(id); // Set userId state
+    //     } else {
+    //         setUserId(null); // Reset userId if not logged in
+    //     }
+    // }, [loggedIn]);
+
     return (
+        // <>
+        // {!loggedIn ? redirect(`/login`) : ""}
         <h1>logged in dashboard</h1>
+        // </>
     )
 }
--- web-client/src/routes/Index.tsx
+++ web-client/src/routes/Index.tsx
@@ -2,31 +2,43 @@ import auth from '../assets/auth'
 // import Login from './Login';
 // import Dashboard from './Dashboard';
 import { useAuth } from '../AuthProvider';
-import { useNavigate } from 'react-router-dom';
+import { redirect, useNavigate, Link } from 'react-router-dom';
 // import Login from './Login';
 import { useState, useEffect } from 'react';
+import Dashboard from './Dashboard';
+
+
+
 
 export default function Index() {
-    const navigate = useNavigate();
-    const loggedIn = useAuth();
-    const [userId, setUserId] = useState<string | null>(null);
+    // const navigate = useNavigate();
+    // const loggedIn = useAuth();
+    // const [userId, setUserId] = useState<string | null>(null);
 
-    useEffect(() => {
-        if (loggedIn) {
-            const id = auth.getId(); // Get the user ID from the token
-            setUserId(id); // Set userId state
-        } else {
-            setUserId(null); // Reset userId if not logged in
-        }
-    }, [loggedIn]);
+    // useEffect(() => {
+    //     if (loggedIn) {
+    //         const id = auth.getId(); // Get the user ID from the token
+    //         setUserId(id); // Set userId state
+    //     } else {
+    //         setUserId(null); // Reset userId if not logged in
+    //     }
+    // }, [loggedIn]);
 
     return (
         <>
-            {loggedIn ?
-                navigate(`/dashboard/${userId}`)
-                :
-                navigate(`/login`)
-            }
+            <div className='grid grid-rows-2'>
+                <Link to="/dashboard" className='content-center text-slate-100 text-2xl md:text-2xl mx-3 hover:text-sky-100 ease-in'>
+                    <button className='bg-slate-900 hover:bg-sky-600 shadow-md shadow-slate-400 rounded-md p-3'>
+                        Dashboard
+                    </button>
+                </Link>
+                <Link to="/login" className='content-center text-slate-100 text-2xl md:text-2xl mx-3 hover:text-sky-100 ease-in'>
+                    <button className='bg-slate-900 hover:bg-sky-600 shadow-md shadow-slate-400 rounded-md p-3'>
+                        Login
+                    </button>
+                </Link>
+
+            </div>
         </>
     )
 }
