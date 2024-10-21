58a25a1adb6186308d944d88a138c5105c772343
David Shaw <david.shaw1242@gmail.com>
2024-10-21 19:13:24 -0400

    a bit of the index is set up but there is an issue with passing the userid to the dashboard

--- web-client/src/routes/Index.tsx
+++ web-client/src/routes/Index.tsx
@@ -12,31 +12,30 @@ import Dashboard from './Dashboard';
 
 export default function Index() {
     // const navigate = useNavigate();
-    // const loggedIn = useAuth();
-    // const [userId, setUserId] = useState<string | null>(null);
+    const { loggedIn } = useAuth();
+    const [userId, setUserId] = useState<string | null>(null);
 
-    // useEffect(() => {
-    //     if (loggedIn) {
-    //         const id = auth.getId(); // Get the user ID from the token
-    //         setUserId(id); // Set userId state
-    //     } else {
-    //         setUserId(null); // Reset userId if not logged in
-    //     }
-    // }, [loggedIn]);
 
     return (
         <>
-            <div className='grid grid-rows-2'>
-                <Link to="/dashboard" className='content-center text-slate-100 text-2xl md:text-2xl mx-3 hover:text-sky-100 ease-in'>
-                    <button className='bg-slate-900 hover:bg-sky-600 shadow-md shadow-slate-400 rounded-md p-3'>
-                        Dashboard
+            <div className='container mx-auto'>
+                <h1 className='flex flex-row justify-center text-3xl font-semibold text-brand py-2'>The Highland Cow</h1>
+                <div className='flex flow-column justify-center text-xl w-3/4 mx-auto text-brand'>Welcome to the Highland Cow!  This app is designed to allow users to scan products with the phone app and then track what and how many of an item that they have.</div>
+            </div>
+            <div className='flex flex-row justify-center'>
+                {loggedIn ? 
+                <Link to={`/dashboard/${userId}`} className='content-center text-slate-100 text-2xl md:text-2xl mx-3 hover:text-sky-100 ease-in'>
+                <button className='bg-slate-900 hover:bg-sky-600 shadow-md shadow-slate-400 rounded-md p-3'>
+                Dashboard
                     </button>
                 </Link>
+                :
                 <Link to="/login" className='content-center text-slate-100 text-2xl md:text-2xl mx-3 hover:text-sky-100 ease-in'>
                     <button className='bg-slate-900 hover:bg-sky-600 shadow-md shadow-slate-400 rounded-md p-3'>
                         Login
                     </button>
                 </Link>
+                }
 
             </div>
         </>
