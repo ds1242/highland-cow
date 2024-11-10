810eec7189b9b55e8341eb7787b40b530ec48a96
David Shaw <david.shaw1242@gmail.com>
2024-11-10 14:59:27 -0500

    added some state variables and added loading and error messages

--- web-client/src/routes/Dashboard.tsx
+++ web-client/src/routes/Dashboard.tsx
@@ -15,12 +15,13 @@ export default function Dashboard() {
     //const navigate = useNavigate();
     const { loggedIn } = useAuth();
     const userToken = auth.getId();
-    const [scanList, setScanList] = useState();
+    const [scanList, setScanList] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    console.log(scanList);
 
     const fetchData = async () => {
-
+        setLoading(true)
         const url = `${domain}${version}/user_scans`
         try {
             const response = await fetch(url, {
@@ -30,21 +31,36 @@ export default function Dashboard() {
                 },
             });
 
-
             if (!response.ok) {
                 throw new Error(`Response status: ${response.status}`);
             }
 
-
             const result = await response.json()
             setScanList(result)
         } catch (error: any) {
-            throw error;
+            setError(error)
+        } finally {
+            setLoading(false)
         }
     }
     useEffect(() => {
         fetchData();
     }, []);
+
+    console.log(scanList);
+
+
+    if (loading) {
+        return (
+            <div> Loading.... </div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div>Error: {error.message} </div>
+        )
+    }
 
     return (
         // <>
