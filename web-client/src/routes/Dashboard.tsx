4074a7a6baec3ac08e99d0eb47b23f89fbb8a53a
David Shaw <david.shaw1242@gmail.com>
2024-11-16 12:32:28 -0500

    added some styling to the buttons

--- web-client/src/routes/Dashboard.tsx
+++ web-client/src/routes/Dashboard.tsx
@@ -105,15 +105,15 @@ export default function Dashboard() {
 
             <div className='overflow-x-auto'>
 
-                <table className='border border-collapse'>
+                <table className='table-auto border border-collapse'>
                     <thead className=''>
                         <tr className='h-14'>
-                            <th className='border border-slate-700 '><p>Brand</p></th>
-                            <th className='border border-slate-700 '><p>Product Name </p></th>
-                            <th className='border border-slate-700 '><p>Description </p></th>
-                            <th className='border border-slate-700 '><p>Quantity </p></th>
-                            <th className='border border-slate-700 '><p>Last Time Updated</p></th>
-                            <th className='border border-slate-700 '><p>Add/Remove</p></th>
+                            <th className='border border-slate-700 bg-slate-400 px-4'><p>Brand</p></th>
+                            <th className='border border-slate-700 bg-slate-400 px-4'><p>Product Name </p></th>
+                            <th className='border border-slate-700 bg-slate-400 px-4'><p>Description </p></th>
+                            <th className='border border-slate-700 bg-slate-400 px-4'><p>Quantity </p></th>
+                            <th className='border border-slate-700 bg-slate-400 px-4'><p>Last Time Updated</p></th>
+                            <th className='border border-slate-700 bg-slate-400 px-4'><p>Add/Remove</p></th>
                         </tr>
                     </thead>
 
@@ -122,20 +122,20 @@ export default function Dashboard() {
                             scanList.length ? (
                                 scanList.map(product => (
                                     <tr className='h-14' key={product.scan_id}>
-                                        <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.brand}</td>
-                                        <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.product_name}</td>
-                                        <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.description}</td>
-                                        <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.quantity}</td>
-                                        <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.updated_at}</td>
-                                        <td className='border border-slate-600 bg-slate-200 px-3 text-center'>
-                                            <button className='px-2 mx-2' onClick={(event) => addOnClick(event, product.scan_id, product.quantity)}>
-                                                <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2Zm0 1.5a8.5 8.5 0 1 0 0 17 8.5 8.5 0 0 0 0-17ZM12 7a.75.75 0 0 1 .75.75v3.5h3.5a.75.75 0 0 1 0 1.5h-3.5v3.5a.75.75 0 0 1-1.5 0v-3.5h-3.5a.75.75 0 0 1 0-1.5h3.5v-3.5A.75.75 0 0 1 12 7Z" fill="#222F3D" />
+                                        <td className='border border-slate-600 bg-slate-200  text-center px-4 '>{product.brand}</td>
+                                        <td className='border border-slate-600 bg-slate-200  text-center px-4 '>{product.product_name}</td>
+                                        <td className='border border-slate-600 bg-slate-200  text-center px-4 '>{product.description}</td>
+                                        <td className='border border-slate-600 bg-slate-200  text-center px-4 '>{product.quantity}</td>
+                                        <td className='border border-slate-600 bg-slate-200  text-center px-4 '>{product.updated_at}</td>
+                                        <td className='border border-slate-600 bg-slate-200  text-center px-4'>
+                                            <button className='px-2 transition ease-out hover:ease-in hover:scale-110 duration-150' onClick={(event) => addOnClick(event, product.scan_id, product.quantity)}>
+                                                <svg width="24" height="24" className='fill-[#222F3D] hover:fill-red-400' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2Zm0 1.5a8.5 8.5 0 1 0 0 17 8.5 8.5 0 0 0 0-17ZM12 7a.75.75 0 0 1 .75.75v3.5h3.5a.75.75 0 0 1 0 1.5h-3.5v3.5a.75.75 0 0 1-1.5 0v-3.5h-3.5a.75.75 0 0 1 0-1.5h3.5v-3.5A.75.75 0 0 1 12 7Z"/>
                                                 </svg>
                                             </button>
-                                            <button className='px-2 mx-2' onClick={(event) => subtractOnClick(event, product.scan_id, product.quantity)}>
-                                                <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2Zm0 1.5a8.5 8.5 0 1 0 0 17 8.5 8.5 0 0 0 0-17Zm4.25 7.75a.75.75 0 0 1 0 1.5h-8.5a.75.75 0 0 1 0-1.5h8.5Z" fill="#222F3D" />
+                                            <button className='px-2 transition ease-out hover:ease-in hover:scale-110 duration-150' onClick={(event) => subtractOnClick(event, product.scan_id, product.quantity)}>
+                                                <svg width="24" height="24" className='fill-[#222F3D] hover:fill-red-400' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2Zm0 1.5a8.5 8.5 0 1 0 0 17 8.5 8.5 0 0 0 0-17Zm4.25 7.75a.75.75 0 0 1 0 1.5h-8.5a.75.75 0 0 1 0-1.5h8.5Z" />
                                                 </svg>
                                             </button>
                                         </td>
