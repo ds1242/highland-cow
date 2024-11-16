2f2d7add4a4976ffda0d5d21ce54a171c4618d1e
David Shaw <david.shaw1242@gmail.com>
2024-11-16 11:27:41 -0500

    finished adding to quantity function

--- web-client/src/routes/Dashboard.tsx
+++ web-client/src/routes/Dashboard.tsx
@@ -1,7 +1,4 @@
-// import auth from '../assets/auth'
-// import Dashboard from './Dashboard';
-// import { getUserScanList } from '../assets/requestUtils';
-// import { useAuth } from '../AuthProvider';
+import { updateScanQuantity } from '../assets/requestUtils';
 import auth from '../assets/auth';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -74,24 +71,30 @@ export default function Dashboard() {
         )
     }
 
-    const addOnClick = (e:any, scan_id:string) => {
+    const addOnClick = async (e:any, scan_id:string, quantity: number) => {
         e.preventDefault();
-        console.log('click ' + scan_id)
+        console.log('click ' + scan_id + ' ' + quantity)
+        let newQuantity = quantity + 1;
+        let quantityUpdate = await updateScanQuantity(scan_id, newQuantity, userToken);
+        if (quantityUpdate == true) {
+            fetchData();
+        };
+
     }
 
-    const subtractOnClick = (e: any, scan_id:string) => {
+    const subtractOnClick = (e: any, scan_id:string, quantity: number) => {
         e.preventDefault();
-        console.log('subtract ' + scan_id);
+        console.log('subtract ' + scan_id + ' ' + quantity);
     }
 
     return (
         <div className='flex justify-center py-5 my-5 h-full'>
 
-            <div className='   overflow-x-auto'>
+            <div className='overflow-x-auto'>
 
                 <table className='border border-collapse'>
                     <thead className=''>
-                        <tr className=''>
+                        <tr className='h-14'>
                             <th className='border border-slate-700 '><p>Brand</p></th>
                             <th className='border border-slate-700 '><p>Product Name </p></th>
                             <th className='border border-slate-700 '><p>Description </p></th>
@@ -105,19 +108,19 @@ export default function Dashboard() {
                         {
                             scanList.length ? (
                                 scanList.map(product => (
-                                    <tr className='' key={product.scan_id}>
+                                    <tr className='h-14' key={product.scan_id}>
                                         <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.brand}</td>
                                         <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.product_name}</td>
                                         <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.description}</td>
                                         <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.quantity}</td>
                                         <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.updated_at}</td>
                                         <td className='border border-slate-600 bg-slate-200 px-3 text-center'>
-                                            <button className='px-2 mx-2' onClick={(event) => addOnClick(event, product.scan_id)}>
+                                            <button className='px-2 mx-2' onClick={(event) => addOnClick(event, product.scan_id, product.quantity)}>
                                                 <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                                     <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2Zm0 1.5a8.5 8.5 0 1 0 0 17 8.5 8.5 0 0 0 0-17ZM12 7a.75.75 0 0 1 .75.75v3.5h3.5a.75.75 0 0 1 0 1.5h-3.5v3.5a.75.75 0 0 1-1.5 0v-3.5h-3.5a.75.75 0 0 1 0-1.5h3.5v-3.5A.75.75 0 0 1 12 7Z" fill="#222F3D" />
                                                 </svg>
                                             </button>
-                                            <button className='px-2 mx-2' onClick={(event) => subtractOnClick(event, product.scan_id)}>
+                                            <button className='px-2 mx-2' onClick={(event) => subtractOnClick(event, product.scan_id, product.quantity)}>
                                                 <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                                     <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2Zm0 1.5a8.5 8.5 0 1 0 0 17 8.5 8.5 0 0 0 0-17Zm4.25 7.75a.75.75 0 0 1 0 1.5h-8.5a.75.75 0 0 1 0-1.5h8.5Z" fill="#222F3D" />
                                                 </svg>
