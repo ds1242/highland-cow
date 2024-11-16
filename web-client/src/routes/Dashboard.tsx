2516bca82fa39f4e377a26396e782f78d7f1c386
David Shaw <david.shaw1242@gmail.com>
2024-11-16 11:05:52 -0500

    figured out how to pass prop values to the click functions

--- web-client/src/routes/Dashboard.tsx
+++ web-client/src/routes/Dashboard.tsx
@@ -74,62 +74,65 @@ export default function Dashboard() {
         )
     }
 
-    const addOnClick = (e:any) => {
+    const addOnClick = (e:any, scan_id:string) => {
         e.preventDefault();
-        console.log('click')
+        console.log('click ' + scan_id)
     }
 
-    const subtractOnClick = (e:any) => {
+    const subtractOnClick = (e: any, scan_id:string) => {
         e.preventDefault();
-        console.log('subtract');
+        console.log('subtract ' + scan_id);
     }
 
     return (
-        <div className='py-5 my-5 flex justify-center h-full'>
+        <div className='flex justify-center py-5 my-5 h-full'>
 
-            <table className='border border-collapse'>
-                <thead className=''>
-                    <tr className=''>
-                        <th className='border border-slate-700 '><p>Brand</p></th>
-                        <th className='border border-slate-700 '><p>Product Name </p></th>
-                        <th className='border border-slate-700 '><p>Description </p></th>
-                        <th className='border border-slate-700 '><p>Quantity </p></th>
-                        <th className='border border-slate-700 '><p>Last Time Updated</p></th>
-                        <th className='border border-slate-700 '><p>Add/Remove</p></th>
-                    </tr>
-                </thead>
+            <div className='   overflow-x-auto'>
 
-                <tbody className=''>
-                {
-                    scanList.length ? (
-                        scanList.map(product => (
-                                <tr className='' key={product.scan_id}>
-                                    <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.brand}</td>
-                                    <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.product_name}</td>
-                                    <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.description}</td>
-                                    <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.quantity}</td>
-                                    <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.updated_at}</td>
-                                    <td className='border border-slate-600 bg-slate-200 px-3 text-center'>
-                                        <button className='px-2 mx-2' onClick={addOnClick}>
-                                            <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2Zm0 1.5a8.5 8.5 0 1 0 0 17 8.5 8.5 0 0 0 0-17ZM12 7a.75.75 0 0 1 .75.75v3.5h3.5a.75.75 0 0 1 0 1.5h-3.5v3.5a.75.75 0 0 1-1.5 0v-3.5h-3.5a.75.75 0 0 1 0-1.5h3.5v-3.5A.75.75 0 0 1 12 7Z" fill="#222F3D" />
-                                            </svg>
-                                        </button>
-                                        <button className='px-2 mx-2' onClick={subtractOnClick}>
-                                            <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2Zm0 1.5a8.5 8.5 0 1 0 0 17 8.5 8.5 0 0 0 0-17Zm4.25 7.75a.75.75 0 0 1 0 1.5h-8.5a.75.75 0 0 1 0-1.5h8.5Z" fill="#222F3D" />
-                                            </svg>
-                                        </button>
-                                    </td>
-                                </tr>
-                        ))
-                    ) : (
-                        <div>No products</div>
-                    )
-                }
-                </tbody>
+                <table className='border border-collapse'>
+                    <thead className=''>
+                        <tr className=''>
+                            <th className='border border-slate-700 '><p>Brand</p></th>
+                            <th className='border border-slate-700 '><p>Product Name </p></th>
+                            <th className='border border-slate-700 '><p>Description </p></th>
+                            <th className='border border-slate-700 '><p>Quantity </p></th>
+                            <th className='border border-slate-700 '><p>Last Time Updated</p></th>
+                            <th className='border border-slate-700 '><p>Add/Remove</p></th>
+                        </tr>
+                    </thead>
 
-            </table>
+                    <tbody className=''>
+                        {
+                            scanList.length ? (
+                                scanList.map(product => (
+                                    <tr className='' key={product.scan_id}>
+                                        <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.brand}</td>
+                                        <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.product_name}</td>
+                                        <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.description}</td>
+                                        <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.quantity}</td>
+                                        <td className='border border-slate-600 bg-slate-200 px-3 text-center '>{product.updated_at}</td>
+                                        <td className='border border-slate-600 bg-slate-200 px-3 text-center'>
+                                            <button className='px-2 mx-2' onClick={(event) => addOnClick(event, product.scan_id)}>
+                                                <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2Zm0 1.5a8.5 8.5 0 1 0 0 17 8.5 8.5 0 0 0 0-17ZM12 7a.75.75 0 0 1 .75.75v3.5h3.5a.75.75 0 0 1 0 1.5h-3.5v3.5a.75.75 0 0 1-1.5 0v-3.5h-3.5a.75.75 0 0 1 0-1.5h3.5v-3.5A.75.75 0 0 1 12 7Z" fill="#222F3D" />
+                                                </svg>
+                                            </button>
+                                            <button className='px-2 mx-2' onClick={(event) => subtractOnClick(event, product.scan_id)}>
+                                                <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2Zm0 1.5a8.5 8.5 0 1 0 0 17 8.5 8.5 0 0 0 0-17Zm4.25 7.75a.75.75 0 0 1 0 1.5h-8.5a.75.75 0 0 1 0-1.5h8.5Z" fill="#222F3D" />
+                                                </svg>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <div>No products</div>
+                            )
+                        }
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     )
 }
